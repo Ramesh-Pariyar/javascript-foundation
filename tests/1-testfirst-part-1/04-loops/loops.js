5596f1c5cf9c70261bd17b850ef2161974002345
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-15 08:32:45 +0545

    testFirst part1 loop

--- tests/1-testfirst-part-1/04-loops/loops.js
+++ tests/1-testfirst-part-1/04-loops/loops.js
@@ -25,10 +25,56 @@ function join(arr, delimiter = "") {
   }
   return finalString;
 }
-function gridGenetor(number) {
+function gridGenerator(number) {
+  let grid = "";
   for (let i = 0; i < number; i++) {
+    let line = "";
     for (let j = 0; j < number; j++) {
-      c;
+      if ((i + j) % 2 === 0) {
+        line += "#";
+      } else {
+        line += " ";
+      }
+    }
+    grid += line + "\n";
+  }
+  return grid;
+}
+
+function paramify(obj) {
+  let Arr = [];
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      Arr.push(`${key}=${obj[key]}`);
     }
   }
+  return Arr.join("&");
+}
+
+function paramifyObjectKeys(obj) {
+  let pairs = [];
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      pairs.push(`${key}=${obj[key]}`);
+    }
+  }
+  pairs.sort();
+
+  return pairs.join("&");
+}
+
+function sort(array) {
+  let arr = array.splice();
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]; // swapping the value
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
