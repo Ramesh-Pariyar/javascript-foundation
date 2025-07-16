d9408eafd964ced055891693d6d89212a578f679
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-16 22:47:38 +0545

    functional programming

--- tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
+++ tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
@@ -1,1 +1,81 @@
 /* eslint-disable no-unused-vars */
+function forEach(arr, callback) {
+  for (i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+  }
+}
+function callback(value) {
+  console.log(value);
+}
+function map(arr, callback) {
+  let arrFinal = [];
+  for (i = 0; i < arr.length; i++) {
+    arrFinal.push(callback(arr[i]));
+  }
+  return arrFinal;
+}
+function doubler(val) {
+  return val * 2;
+}
+
+function filter(arr, callback) {
+  let finalResult = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      finalResult.push(arr[i]);
+    }
+  }
+  return finalResult;
+}
+const evenFilter = (element) => element % 2 === 0;
+const oddFilter = (element) => !evenFilter(element);
+// const oddFilter = (element) => element % 2 === 1
+
+function includes(collection, checkingValue) {
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      if (collection[i] === checkingValue) {
+        return true;
+      }
+    }
+  } else if (typeof collection === "object" && collection !== null) {
+    for (let key in collection) {
+      if (collection[key] === checkingValue) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+function reduce(arr, start, combiner) {
+  let result = start;
+  for (i = 0; i < arr.length; i++) {
+    result = combiner(result, arr[i]);
+  }
+  return result;
+}
+function countWords(total, sentence) {
+  const wordCount = sentence.split(" ").filter((word) => word !== "").length;
+  return total + wordCount;
+}
+function sum(arr) {
+  return reduce(arr, 0, (a, b) => a + b);
+}
+function every(arr, callback) {
+  for (i = 0; i < arr.length; i++) {
+    if (!callback(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+function some(array, test) {
+  if (typeof test !== "function") return false;
+
+  for (let i = 0; i < array.length; i++) {
+    if (test(array[i])) {
+      return true;
+    }
+  }
+  return false;
+}
--- tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -1,1 +1,80 @@
 /* eslint-disable no-unused-vars */
+function indexAndValue(arr) {
+  return arr.map((value, index) => {
+    return { index, value };
+  });
+}
+function capitalize(str) {
+  return str.toUpperCase();
+}
+
+function swapCase(sentence) {
+  return sentence
+    .split(" ")
+    .map((word, index) => {
+      return index % 2 === 0 ? capitalize(word) : word;
+    })
+    .join(" ");
+}
+function extensionSearch(ext, files) {
+  return files.filter((filename) => filename.endsWith(`.${ext}`));
+}
+function getPopulation(countries, selectedCountries) {
+  return countries.reduce((total, country) => {
+    if (
+      selectedCountries.length === 0 ||
+      selectedCountries.includes(country.name)
+    ) {
+      return total + country.population;
+    }
+    return total;
+  }, 0);
+}
+function keyifyArrayOfObjects(key, array) {
+  return array.reduce((acc, obj) => {
+    acc[obj[key]] = obj;
+    return acc;
+  }, {});
+}
+function powerLevelAverage(superheroes) {
+  const totalPower = superheroes.reduce(
+    (sum, hero) => sum + hero.powerLevel,
+    0
+  );
+  return Math.round(totalPower / superheroes.length);
+}
+function mapReduce(array, callback) {
+  return array.reduce((accumulator, currentValue, index, originalArray) => {
+    accumulator.push(callback(currentValue, index, originalArray));
+    return accumulator;
+  }, []);
+}
+function filterReduce(array, callback) {
+  return array.reduce((accumulator, currentValue, index, originalArray) => {
+    if (callback(currentValue, index, originalArray)) {
+      accumulator.push(currentValue);
+    }
+    return accumulator;
+  }, []);
+}
+function inYourBudget(maxPrice, items) {
+  return items
+    .filter((item) => item.price <= maxPrice)
+    .map((item) => item.item);
+}
+function separateAndReturnNames(superheroes, key, maxLength) {
+  superheroes.map((hero) => {
+    const [firstName, lastName] = hero.name.split(" ");
+    hero.firstName = firstName;
+    hero.lastName = lastName;
+  });
+
+  return superheroes
+    .map((hero) => hero[key])
+    .filter((name) => name.length <= maxLength);
+}
+function priorityTodoDuration(todoList) {
+  return todoList
+    .filter((task) => task.priority === "high")
+    .reduce((sum, task) => sum + task.duration, 0);
+}
