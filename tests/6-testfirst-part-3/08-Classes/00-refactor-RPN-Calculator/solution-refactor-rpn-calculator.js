9138589a2399cfbb95833c82a7012d39de62be21
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-16 22:48:14 +0545

    classes practice

--- tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,43 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+  push(value) {
+    this.stack.push(value);
+  }
+  plus() {
+    this._requireStackLength(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a + b);
+  }
+  minus() {
+    this._requireStackLength(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a - b);
+  }
+  times() {
+    this._requireStackLength(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a * b);
+  }
+  divide() {
+    this._requireStackLength(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a / b);
+  }
+  value() {
+    return this.stack[this.stack.length - 1];
+  }
+  _requireStackLength(n) {
+    if (this.stack.length < n) {
+      throw "rpnCalculatorInstance is empty";
+    }
+  }
+}
--- tests/6-testfirst-part-3/08-Classes/01-classes/classes.js
+++ tests/6-testfirst-part-3/08-Classes/01-classes/classes.js
@@ -1,1 +1,44 @@
 /* eslint-disable no-unused-vars */
+class Mammal {
+  constructor(name) {
+    this.name = name;
+    this.offspring = [];
+  }
+
+  sayHello() {
+    return `My name is ${this.name}, I'm a ${this.constructor.name}`;
+  }
+
+  haveBaby() {
+    const baby = new this.constructor(`Baby ${this.name}`);
+    this.offspring.push(baby);
+    return baby;
+  }
+}
+
+class Dog extends Mammal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
+
+  bark() {
+    return "RUFF RUFF";
+  }
+
+  haveBaby(breed) {
+    const baby = new Dog(`Baby ${this.name}`, breed);
+    this.offspring.push(baby);
+    return baby;
+  }
+}
+class Cat extends Mammal {
+  constructor(name, color) {
+    super(name);
+    this.color = color;
+  }
+
+  meow() {
+    return "meow";
+  }
+}
